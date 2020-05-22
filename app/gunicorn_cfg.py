b1b6bdc44ace2aefb19c6d48b61b2b632ce806f3
Merijn Bol <merijn@merijnbol.nl>
2020-05-22 15:49:01 +0200

    change gunicorn settings.

--- app/gunicorn_cfg.py
+++ app/gunicorn_cfg.py
@@ -1,6 +1,5 @@
-command = "/home/git/.local/share/virtualenvs/django_app-ynGG7eLT/bin/gunicorn"
-pythonpath = "/srv/django_app/app"
+# command = "/home/git/.local/share/virtualenvs/django_app-ynGG7eLT/bin/gunicorn"
+# pythonpath = "/srv/django_app/app"
 bind = "127.0.0.1:8001"
-workers = 5
-user = "nobody"
-
+workers = 2
+# user = "nobody"
