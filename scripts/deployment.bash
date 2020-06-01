223a97e838710090b5c87dc35a61f8417b4a53d4
Merijn Bol <merijn@merijnbol.nl>
2020-06-01 17:43:44 +0200

    Fix the deployment script paths.

--- scripts/deployment.bash
+++ scripts/deployment.bash
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # cd to project folder
-cd /srv/django_app/app
+cd /srv/django_app
 
 # activate venv and load all environment variables
 source .env
 
 #try to kill old process
-pid=`cat gunicorn_pids`
+pid=`cat app/gunicorn_pids`
 echo "Killing old process with PID = $pid"
 kill $pid
 
 # run pre deploy maintenance tasks
 pip install -r requirements.txt
+cd app
 python manage.py migrate
 python manage.py collectstatic --noinput
 # start the detached gunicorn process per gunicorn_cfg settings via pipenv.
